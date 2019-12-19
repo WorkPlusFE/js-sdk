0c6241f9bdcc01f45c67fbb4dbebe20e1cbb853d
Masonz <masonzone@gmail.com>
2019-12-19 23:31:30 +0800

    build: fix build failure

--- lib/@w6s/index.d.ts
+++ lib/@w6s/index.d.ts
@@ -1,4 +1,6 @@
 import WPStorage from './storage';
+import * as WPUtils from './utils';
 export declare const version = "0.0.1";
 export declare const native: {};
 export declare const Storage: typeof WPStorage;
+export declare const utils: typeof WPUtils;
---
+++ lib/@w6s/utils/index.d.ts
@@ -0,0 +1,6 @@
+export * from './is';
+/**
+ * 休眠函数
+ * @param time 时间，单位毫秒
+ */
+export declare const sleep: (time: number) => Promise<void>;
