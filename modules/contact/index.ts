de1d90df7c8f2aae0a506360439c95b65355eb5a
何镇豪 <elvis4study@gmail.com>
2019-12-26 14:19:32 +0800

    chore: 添加user模块方法注释

--- modules/contact/index.ts
+++ modules/contact/index.ts
@@ -25,6 +25,13 @@ import {
 
 const SERVICE_KEY = 'WorkPlus_Contact';
 
+/**
+ * 单选联系人
+ *
+ * @export
+ * @param {ContactOption<SingleContactParams, SingleContactRes>} [options]
+ * @returns {Promise<SingleContactRes>}
+ */
 export function getSingleContact(
   options?: ContactOption<SingleContactParams, SingleContactRes>,
 ): Promise<SingleContactRes> {
@@ -37,6 +44,13 @@ export function getSingleContact(
   );
 }
 
+/**
+ * 打开当前组织的联系人列表，选择多个联系人
+ *
+ * @export
+ * @param {ContactOption<ContactsParams, ContactsRes>} [options]
+ * @returns {Promise<ContactsRes>}
+ */
 export function getContacts(
   options?: ContactOption<ContactsParams, ContactsRes>,
 ): Promise<ContactsRes> {
@@ -49,6 +63,13 @@ export function getContacts(
   );
 }
 
+/**
+ * 打开当前组织的联系人列表，选择多个联系人
+ *
+ * @export
+ * @param {ContactOption<EmployeesFromOrgParams, CurrentEmployeeInfoRes>} [options]
+ * @returns {Promise<CurrentEmployeeInfoRes>}
+ */
 export function getEmployeesFromCurrentOrg(
   options?: ContactOption<EmployeesFromOrgParams, CurrentEmployeeInfoRes>,
 ): Promise<CurrentEmployeeInfoRes> {
@@ -61,6 +82,13 @@ export function getEmployeesFromCurrentOrg(
   );
 }
 
+/**
+ * 获取当前登录用户详细信息
+ *
+ * @export
+ * @param {ContactOption<CurrentUserInfoParams, CurrentUserInfoRes>} [options]
+ * @returns {Promise<CurrentUserInfoRes>}
+ */
 export function getCurrentUserInfo(
   options?: ContactOption<CurrentUserInfoParams, CurrentUserInfoRes>,
 ): Promise<CurrentUserInfoRes> {
@@ -73,6 +101,13 @@ export function getCurrentUserInfo(
   );
 }
 
+/**
+ * 获取当前登录用户的某个组织里的雇员信息
+ *
+ * @export
+ * @param {ContactOption<CurrentEmployeeInfoParams, CurrentEmployeeInfoRes>} [options]
+ * @returns {Promise<CurrentEmployeeInfoRes>}
+ */
 export function getCurrentEmployeeInfo(
   options?: ContactOption<CurrentEmployeeInfoParams, CurrentEmployeeInfoRes>,
 ): Promise<CurrentEmployeeInfoRes> {
@@ -85,6 +120,13 @@ export function getCurrentEmployeeInfo(
   );
 }
 
+/**
+ * 轻应用输入userId，domianId, 跳转到workplus与该用户的聊天页面
+ *
+ * @export
+ * @param {ContactOption<ShowUserChatViewByUserParams, unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function showUserChatViewByUser(
   options?: ContactOption<ShowUserChatViewByUserParams, unknown>,
 ): Promise<unknown> {
@@ -97,6 +139,13 @@ export function showUserChatViewByUser(
   );
 }
 
+/**
+ * 获取手机通讯录联系人的列表
+ *
+ * @export
+ * @param {ContactOption<[], unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function getMobileContacts(options?: ContactOption<[], unknown>): Promise<unknown> {
   return SDK.sendEvent<[], unknown, unknown>(
     SERVICE_KEY,
@@ -107,6 +156,13 @@ export function getMobileContacts(options?: ContactOption<[], unknown>): Promise
   );
 }
 
+/**
+ * 通过用户域id和用户id获取这个用户的详细信息
+ *
+ * @export
+ * @param {ContactOption<GetUserInfoByUserIdParams, GetUserInfoByUserIdRes>} [options]
+ * @returns {Promise<GetUserInfoByUserIdRes>}
+ */
 export function getUserInfoByUserId(
   options?: ContactOption<GetUserInfoByUserIdParams, GetUserInfoByUserIdRes>,
 ): Promise<GetUserInfoByUserIdRes> {
@@ -119,6 +175,13 @@ export function getUserInfoByUserId(
   );
 }
 
+/**
+ * 通过用户username展示这个用户的详情页面
+ *
+ * @export
+ * @param {ContactOption<ShowUserInfoByUsernameParams, unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function showUserInfoByUsername(
   options?: ContactOption<ShowUserInfoByUsernameParams, unknown>,
 ): Promise<unknown> {
@@ -131,6 +194,13 @@ export function showUserInfoByUsername(
   );
 }
 
+/**
+ * 通过群组id打开该群组的聊天页面，并且可以自定义发送应用所支持的消息
+ *
+ * @export
+ * @param {ContactOption<OpenDiscussionByIdParams, unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function openDiscussionById(
   options?: ContactOption<OpenDiscussionByIdParams, unknown>,
 ): Promise<unknown> {
@@ -143,6 +213,13 @@ export function openDiscussionById(
   );
 }
 
+/**
+ * 通过调用原生选人界面，选择人员，创建群组，并且返回相关群组信息
+ *
+ * @export
+ * @param {ContactOption<[], unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function createDiscussionChat(options?: ContactOption<[], unknown>): Promise<unknown> {
   return SDK.sendEvent<[], unknown, unknown>(
     SERVICE_KEY,
@@ -153,6 +230,13 @@ export function createDiscussionChat(options?: ContactOption<[], unknown>): Prom
   );
 }
 
+/**
+ * 通过应用id和orgId的，打开改组织下某个应用聊天界面
+ *
+ * @export
+ * @param {ContactOption<ShowAppChatViewParams[], unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function showAppChatViewById(
   options?: ContactOption<ShowAppChatViewParams[], unknown>,
 ): Promise<unknown> {
@@ -165,6 +249,13 @@ export function showAppChatViewById(
   );
 }
 
+/**
+ * 打开搜索页面
+ *
+ * @export
+ * @param {ContactOption<[], unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function searchInApp(options?: ContactOption<[], unknown>): Promise<unknown> {
   return SDK.sendEvent<[], unknown, unknown>(
     SERVICE_KEY,
@@ -175,6 +266,13 @@ export function searchInApp(options?: ContactOption<[], unknown>): Promise<unkno
   );
 }
 
+/**
+ * 根据组织id打开该组织下的应用列表页面
+ *
+ * @export
+ * @param {ContactOption<ShowAppListByIdParams, unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function showAppListById(
   options?: ContactOption<ShowAppListByIdParams, unknown>,
 ): Promise<unknown> {
@@ -187,6 +285,13 @@ export function showAppListById(
   );
 }
 
+/**
+ * 打开通用选择人员界面, 多选用户或者雇员
+ *
+ * @export
+ * @param {ContactOption<SelectContactsParams, unknown>} [options]
+ * @returns {Promise<unknown>}
+ */
 export function selectContacts(
   options?: ContactOption<SelectContactsParams, unknown>,
 ): Promise<unknown> {
@@ -199,6 +304,13 @@ export function selectContacts(
   );
 }
 
+/**
+ * 根据群组 id, 打开群组成员界面, 并进行选择
+ *
+ * @export
+ * @param {ContactOption<SelectDiscussionMembersParams, SelectDiscussionMembersRes>} [options]
+ * @returns {Promise<SelectDiscussionMembersRes>}
+ */
 export function selectDiscussionMembers(
   options?: ContactOption<SelectDiscussionMembersParams, SelectDiscussionMembersRes>,
 ): Promise<SelectDiscussionMembersRes> {
@@ -211,6 +323,13 @@ export function selectDiscussionMembers(
   );
 }
 
+/**
+ * 获取选人缓存数据
+ *
+ * @export
+ * @param {ContactOption<[], SelectContactsCacheRes>} [options]
+ * @returns {Promise<SelectContactsCacheRes>}
+ */
 export function selectContactsCache(
   options?: ContactOption<[], SelectContactsCacheRes>,
 ): Promise<SelectContactsCacheRes> {
--- modules/types/contact.ts
+++ modules/types/contact.ts
@@ -6,7 +6,7 @@ export interface SingleContactParams {
 export interface ContactOption<D, R> {
   data: D[];
   success: (res: R) => void;
-  fail: (err: any) => void;
+  fail: (err: unknown) => void;
 }
 
 export interface SingleContactRes {
