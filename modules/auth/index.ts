b38f701ccadd53b7a8c1eeda8f1820260a8edefe
hejianxian <531601727@qq.com>
2020-08-18 23:47:05 +0800

    fix: auth index.ts

--- modules/auth/index.ts
+++ modules/auth/index.ts
@@ -1,11 +1,8 @@
-import getLoginUserInfo from './getLoginUserInfo';
 import getServerInfo from './getServerInfo';
 import getTenantID from './getTenantID';
 import getUserTicket from './getUserTicket';
 
 export {
-  /** 获取当前登录用户最基本详情 */
-  getLoginUserInfo,
   /** 获取当前app请求后台api地址 */
   getServerInfo,
   /** 获取当前的租户id */
