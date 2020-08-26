919b17e684bb0891c94427097d8e0443371c4c52
hejianxian <531601727@qq.com>
2020-08-26 14:07:49 +0800

    chore: prettier copyText api

--- modules/webview/copyText.ts
+++ modules/webview/copyText.ts
@@ -3,7 +3,7 @@ import { WORKPLUS_WEBVIEW } from '../constants';
 import { ExecOptions } from '../types/core';
 import { CopyTextParams } from '../types/webview';
 
-interface CopyTextOptions extends ExecOptions<void, void>, CopyTextParams {};
+interface CopyTextOptions extends ExecOptions<void, void>, CopyTextParams {}
 
 /**
  * 复制文本
@@ -12,12 +12,8 @@ interface CopyTextOptions extends ExecOptions<void, void>, CopyTextParams {};
  * @module webview
  * @returns {Promise<void>}
  */
-function copyText(options: CopyTextOptions){
-  return core.execSync(
-    WORKPLUS_WEBVIEW,
-    'copyText',
-    [options.text],
-  );
+function copyText(options: CopyTextOptions) {
+  return core.execSync(WORKPLUS_WEBVIEW, 'copyText', [options.text]);
 }
 
 export default copyText;
