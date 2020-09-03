240d30b724e18afbd9c266c65daecee1d3f11706
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-03 16:33:46 +0000

    Deploy WorkPlusFE/js-sdk to github.com/WorkPlusFE/js-sdk.git:gh-pages

---
+++ assets/js/8.3eae67b5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{510:function(t,e,n){"use strict";n.r(e);var s=n(37),o=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("FeaturesSection")],1)}),[],!1,null,null,null);e.default=o.exports}}]);
