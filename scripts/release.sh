d636ffa9a3a6e9885a5181c5bff791faa5f865f8
hejianxian <531601727@qq.com>
2020-09-04 12:57:49 +0800

    chore: add release script

---
+++ scripts/release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+if [[ -z $1 ]]; then
+  echo "Enter new version: "
+  read -r VERSION
+else
+  VERSION=$1
+fi
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Releasing $VERSION ..."
+
+  # lint
+  npm run lint
+
+  # build
+  VERSION=$VERSION 
+  npm version "$VERSION"
+  npm run build
+
+  # commit
+  git add .
+  git commit -m "build: build $VERSION"
+  # generate release note
+  npm run release:note
+  # tag version
+  npm version "$VERSION" --message "build: release $VERSION"
+
+  # publish
+  git push origin refs/tags/v"$VERSION"
+  git push
+  
+  npm publish
+fi
