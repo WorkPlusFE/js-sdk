9640f7c354763b4bc4113cd381abe693f231e505
hejianxian <531601727@qq.com>
2020-09-08 14:30:11 +0800

    docs: add sdk example qrcode

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import CodeWrapper from '@w6s/vuepress-code-wrapper';
 
 const host = process.env.NODE_ENV === 'production' ?
-  'https://js-sdk.workplus.io/demo' :
+  'https://open.workplus.io/sdk-demo/' :
   'http://172.16.1.66:8081';
 
 export default ({
--- docs/js-sdk/overview/demo.md
+++ docs/js-sdk/overview/demo.md
@@ -1,3 +1,9 @@
 # 例子及资源
 
-暂无！
+## 在线 demo
+
+请使用 WorkPlus 扫一扫，查看在线 Demo：
+
+<p class="w6s-image">
+  <img :src="$withBase('/js-sdk/sdk-demo-qrcode.png')" alt="sdk demo" width="250px" />
+</p>
