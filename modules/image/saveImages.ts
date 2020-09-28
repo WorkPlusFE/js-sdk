9adab4c557f84f767b8c7854727b880b83cebcb4
hejianxian <531601727@qq.com>
2020-09-28 14:47:44 +0800

    refactor: optimize saveImages api

--- modules/image/saveImages.ts
+++ modules/image/saveImages.ts
@@ -3,7 +3,7 @@ import { WORKPLUS_IMAGE } from '../constants';
 import { SaveImageItem } from '../types/image';
 import { ExecOptions } from '../types/core';
 
-export type  SaveImagesOptions = SaveImageItem & ExecOptions<string, void>;
+export type SaveImagesOptions = SaveImageItem & ExecOptions<string, void>;
 
 /**
  * 保存图片
@@ -15,8 +15,8 @@ export type  SaveImagesOptions = SaveImageItem & ExecOptions<string, void>;
 function saveImages(options: SaveImagesOptions): Promise<string> {
   const { success, fail, ...arg } = options;
   return core.exec<SaveImageItem, string, void>(
-    WORKPLUS_IMAGE, 
-    'saveImages', 
+    WORKPLUS_IMAGE,
+    'saveImages',
     [arg],
     success,
     fail,
