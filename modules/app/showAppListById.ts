6194e3a03595dd3077d08787a15bd7230c79805d
hejianxian <531601727@qq.com>
2020-10-11 00:35:59 +0800

    refactor: show app list by id api

--- modules/app/showAppListById.ts
+++ modules/app/showAppListById.ts
@@ -1,7 +1,7 @@
 import * as core from '../core';
 import { ShowAppListByIdParams } from '../types/contact';
 
-import { WORKPLUS_CONTACT } from '../constants';
+import { WORKPLUS_APP_STORE } from '../constants';
 
 /**
  * 根据组织id打开该组织下的应用列表页面
@@ -11,5 +11,5 @@ import { WORKPLUS_CONTACT } from '../constants';
  * @returns {void}
  */
 export default function showAppListById(options: ShowAppListByIdParams): void {
-  return core.execSync<ShowAppListByIdParams>(WORKPLUS_CONTACT, 'showAppListById', [options]);
+  return core.execSync<ShowAppListByIdParams>(WORKPLUS_APP_STORE, 'showAppListById', [options]);
 }
