437aebe4a88b28230536d4a9066e355217e5838c
Hejx <531601727@qq.com>
2021-12-22 00:25:14 +0800

    feat(config): update checkApi description

--- modules/config/checkApi.ts
+++ modules/config/checkApi.ts
@@ -13,7 +13,7 @@ export type CheckApiOptions = CheckApiParams & ExecOptions<CommonApiRes, void>;
 
 /**
  * 接口检测
- * @description 检验是否存在 Cordova 插件和方法
+ * @description 检验是否存在 Cordova 插件和方法，该方面不能直接检查 SDK 的方法，需要使用原始 Cordova 接口文档的接口命名参数。
  * @param {ConfigOptions} [options]
  * @module config
  * @returns {CommonApiRes}
