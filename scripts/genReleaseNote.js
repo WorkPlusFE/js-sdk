eb8c5e437434ea4dbd6ef55cbf79621efbd74e4a
Hejx <531601727@qq.com>
2021-12-21 17:16:47 +0800

    build(npm): optimize release note output

--- scripts/genReleaseNote.js
+++ scripts/genReleaseNote.js
@@ -1,6 +1,6 @@
-const cc = require('conventional-changelog');
+const version = process.argv[2] || process.env.VERSION;
 
-const { version } = require('../package.json');
+const cc = require('conventional-changelog');
 const file = `./release_note/RELEASE_NOTE${version ? `_${version}` : ``}.md`;
 const fileStream = require('fs').createWriteStream(file);
 
