a7aa55a95fa3696ae747fa95673767904f22bf8d
Hejx <531601727@qq.com>
2021-12-22 00:41:00 +0800

    build: build 2.0.0-beta.6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -15,7 +15,7 @@ import * as Config from './config';
 import * as Dialog from './dialog';
 import * as Storage from './storage';
 /** WorkPlus SDK 版本 */
-export declare const version = "2.0.0-beta.5";
+export declare const version = "2.0.0-beta.6";
 export declare const native: {};
 /** 图像接口 */
 export declare const image: {
---
+++ release_note/RELEASE_NOTE_2.0.0-beta.6.md
@@ -0,0 +1,9 @@
+# [2.0.0-beta.6](https://github.com/WorkPlusFE/js-sdk/compare/v2.0.0-beta.5...v2.0.0-beta.6) (2021-12-21)
+
+
+### Features
+
+* **build:** update release note generator ([86a155c](https://github.com/WorkPlusFE/js-sdk/commit/86a155cdbd6178f778719c1e585467b792ec7737))
+
+
+
