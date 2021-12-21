ed3830db9fa8a01a3725c56cb9e8ba6babd3007f
Hejx <531601727@qq.com>
2021-12-22 00:30:36 +0800

    build: optimize replace version script

--- scripts/genReleaseNote.js
+++ scripts/genReleaseNote.js
@@ -1,8 +1,9 @@
-const version = process.argv[2] || process.env.VERSION;
-
 const cc = require('conventional-changelog');
+
 const file = `./release_note/RELEASE_NOTE.md`;
 const fileStream = require('fs').createWriteStream(file);
+
+const version = process.argv[2] || process.env.VERSION;
 
 cc({
   preset: 'angular',
--- scripts/replaceVersion.js
+++ scripts/replaceVersion.js
@@ -1,12 +1,12 @@
-const pkg = require('../package.json');
 const path = require('path');
 const replace = require('replace-x').default;
 
+const version = process.argv[2] || process.env.VERSION;
 const dist = path.resolve(__dirname, '../dist/');
 
 replace({
   paths: [dist],
   recursive: true,
   regex: '__VERSION__',
-  replacement: `${pkg.version}`,
+  replacement: `${version}`,
 });
